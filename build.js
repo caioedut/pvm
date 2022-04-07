1bbd07fbc5c1c0c616a4d105dee87fcef899b1f8
Caio Teixeira <caioedut@gmail.com>
2022-04-07 18:04:44 -0300

    Build

--- build.js
+++ build.js
@@ -12,11 +12,11 @@ const { execSync } = require('child_process');
   execSync('yarn global add pkg');
   execSync('yarn install --check-files');
 
-  // Update package version
-  execSync('yarn version --patch', options);
-
   // Compile to .exe
   execSync(`pkg . -o bin/pvm`, options);
 
   fs.copyFileSync('bin/pvm.exe', 'setup/pvm.exe');
+
+  // Update package version
+  execSync('yarn version --patch', options);
 })();
