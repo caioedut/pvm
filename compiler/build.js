26257ff479bc8917cd227f0fcef5984afb1e7480
Caio Teixeira <caioedut@gmail.com>
2022-04-10 17:09:32 -0300

    Release 0.1.2

---
+++ compiler/build.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+
+(async function main() {
+  console.clear();
+
+  const options = { stdio: 'inherit' };
+
+  console.log('Building .exe file...');
+
+  // Install packages
+  execSync('yarn global add pkg');
+  execSync('yarn install --check-files');
+
+  // Compile to .exe
+  execSync(`pkg . -o bin/pvm`, options);
+
+  // Update package version
+  // execSync('yarn version --patch', options);
+
+  // Done
+  console.log('File pvm.exe (CLI) was created on directory "bin".');
+})();
---
+++ compiler/choco.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const { execSync } = require('child_process');
+
+(async function main() {
+  const options = { stdio: 'inherit', cwd: 'chocolatey' };
+
+  console.log('Pushing to Chocolatey...');
+
+  // Push to Chocolatey
+  execSync(`choco push --key=${process.env.CHOCO_API_KEY}`, options);
+
+  // Done
+  console.log('File pvm-setup.exe was sent to Chocolatey.');
+})();
